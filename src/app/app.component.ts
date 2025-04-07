74f86e9aa2d45344b0cf09ca6cc53f5b5282afab
Harshal-Meher <harshalmeher294@gmail.com>
2025-04-07 19:00:45 +0530

    Auto Suggest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'autoComplete';
+export class AppComponent implements OnInit {
+ 
+  candidateOptions:any[]= [
+    { name: 'Harshal' }, { name: 'Abhi' }, { name: 'Roshan' }, { name: 'Jay' }
+    ];
+
+    filteredOptions!:Observable<any[]>;
+    myControl = new FormControl(''); 
+
+    ngOnInit(): void {
+      this.filteredOptions = this.myControl.valueChanges.pipe(
+        startWith(''),
+        map(value => this._filter(value||''))
+      )
+    }
+    
+    displayOptions(item:any){
+      return item?item.name:undefined;
+    }
+
+    private _filter(value:string):string[] {
+     const filteredValue = value.toLowerCase();
+     return this.candidateOptions.filter(option => option.name.toLowerCase().includes(filteredValue));
+    }
+
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatFormField,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
